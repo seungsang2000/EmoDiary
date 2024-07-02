742c8acd48f5ff9009bad39912b51791779fb3d7
seungsang <rovin000@hansung.ac.kr>
2024-07-02 23:16:31 +0900

    s3

--- src/main/java/com/toy/project/emodiary/s3/dto/PresignedUrlDto.java
+++ src/main/java/com/toy/project/emodiary/s3/dto/PresignedUrlDto.java
@@ -2,8 +2,10 @@ package com.toy.project.emodiary.s3.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class PresignedUrlDto {
     private String fileName;
--- src/main/java/com/toy/project/emodiary/s3/dto/PresignedUrlRequestDto.java
+++ src/main/java/com/toy/project/emodiary/s3/dto/PresignedUrlRequestDto.java
@@ -1,8 +1,10 @@
 package com.toy.project.emodiary.s3.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PresignedUrlRequestDto {
     private String filePath;
     private String fileName;
